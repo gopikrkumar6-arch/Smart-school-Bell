10f53c099cf818792c81c90835571b579513a574
Simrankumari1603 <simarn2936kumari@gmail.com>
2026-01-28 23:55:12 +0530

    history swipe away2

--- android/app/src/main/java/com/smartschoolbell/app/MainActivity.java
+++ android/app/src/main/java/com/smartschoolbell/app/MainActivity.java
@@ -1,13 +1,5 @@
 package com.smartschoolbell.app;
 
-import android.content.Intent;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onTaskRemoved(Intent rootIntent) {
-        super.onTaskRemoved(rootIntent);
-        // This helps the OS understand the app intends to keep running 
-        // even if the task is removed from the recent apps list.
-    }
-}
+public class MainActivity extends BridgeActivity {}
